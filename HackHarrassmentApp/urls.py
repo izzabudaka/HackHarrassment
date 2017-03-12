9c9284ed91f84cd8b80539a7922ada8245330758
Izz Abudaka <izz.abudaka@gmail.com>
2017-03-12 08:20:29 +0000

    fixed merge issue

--- HackHarrassmentApp/urls.py
+++ HackHarrassmentApp/urls.py
@@ -8,12 +8,8 @@ urlpatterns = [
     url(r'^connections/', views.get_relations),
     url(r'^users/', views.get_users),
     url(r'^latest/', views.get_latest_messages),
-<<<<<<< HEAD
-    url(r'^post/', views.post_message)
-=======
     url(r'^post', views.post_message),
     url(r'^create_user', views.create_user),
     url(r'^last_messages', views.last_messages),
     url(r'^sms', views.on_incoming_sms)
->>>>>>> b5970a934dc8d0d99095f59a8364dfe10ccdc9d6
 ]
